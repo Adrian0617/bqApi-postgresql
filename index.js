792de7b79a099f434f3d1b757efb412c2d85cbb3
adrian <adriarias243@gmail.com>
2023-08-04 12:12:33 -0500

    refactor: schema adding tearUp and tearDown

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const config = require('./config');
-const authMiddleware = require('./middleware/auth');
-const errorHandler = require('./middleware/error');
-const routes = require('./routes');
-const pkg = require('./package.json');
-const { createTables } = require('./models/Schemas');
+const express = require("express");
+const config = require("./config");
+const authMiddleware = require("./middleware/auth");
+const errorHandler = require("./middleware/error");
+const routes = require("./routes");
+const pkg = require("./package.json");
+
 
 const { port, secret } = config;
 const app = express();
 
-createTables()
-
-app.set('config', config);
-app.set('pkg', pkg);
+app.set("config", config);
+app.set("pkg", pkg);
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
@@ -31,3 +29,5 @@ routes(app, (err) => {
     console.info(`App listening on port ${port}`);
   });
 });
+
+module.exports = {app}
--- models/Schemas.js
+++ models/Schemas.js
@@ -1,38 +1,72 @@
-const { db } = require("../connect")
+const { db } = require("../connect");
+const { sql } = require("kysely");
 
-async function createTables() {
-    try {
-      await db.schema
-      .createTable('products')
-      .addColumn('id', 'serial', (col) => col.primaryKey())
-      .addColumn('price', 'varchar', (col) => col.notNull())
-      .addColumn('name', 'varchar', (col) => col.notNull())
-      .execute()  
-    } catch (error) {
-      // console.log("products error",error);
-    }
-    try {
-      await db.schema
-      .createTable('users')
-      .addColumn('id', 'serial', (col) => col.primaryKey())
-      .addColumn('name', 'varchar', (col) => col.notNull())
-      .addColumn('age', 'varchar', (col) => col.notNull())
-      .execute()  
-    } catch (error) {
-      // console.log('users errors');
-    } 
-    try {
-      await db.schema
-      .createTable('admins')
-      .addColumn('id', 'serial', (col) => col.primaryKey())
-      .addColumn('name', 'varchar', (col) => col.notNull())
-      .addColumn('age', 'varchar', (col) => col.notNull())
-      .execute()  
-    } catch (error) {
-      // console.log('admins errors');
-    }  
+async function tearUp() {
+  try {
+    await db.schema
+      .createTable("products")
+      .addColumn("id", "serial", (col) => col.primaryKey())
+      .addColumn("price", "varchar", (col) => col.notNull())
+      .addColumn("name", "varchar", (col) => col.notNull())
+      .addColumn("createdAt", sql`timestamp with time zone`, (cb) =>
+        cb.defaultTo(sql`current_timestamp`)
+      )
+      .execute();
+    addProducts(db)
+  } catch (error) {
+    // console.log("products error",error);
   }
+  try {
+    await db.schema
+      .createTable("users")
+      .addColumn("id", "serial", (col) => col.primaryKey())
+      .addColumn("name", "varchar", (col) => col.notNull())
+      .addColumn("role", "varchar", (col) => col.notNull())
+      .addColumn("createdAt", sql`timestamp with time zone`, (cb) =>
+        cb.defaultTo(sql`current_timestamp`)
+      )
+      .execute();
+    addUser(db);
+  } catch (error) {
+    // console.log('users errors');
+  }
+}
 
-  module.exports = {
-    createTables
-  }
+async function tearDown(db) {
+  await db.schema.dropTable("users").ifExists().execute();
+  await db.schema.dropTable("products").ifExists().execute();
+}
+
+async function addUser(db) {
+  const user = await db
+    .insertInto("users")
+    .values([
+      {
+        name: "adrian",
+        role: "admin",
+      },
+    ])
+    .returning(["name", "role"])
+    .execute();
+  return user[0].id;
+}
+
+async function addProducts(db) {
+  const product = await db
+    .insertInto("products")
+    .values([
+      {
+        name: "snadwich",
+        price: 2330,
+      },
+    ])
+    .returning(["id", "name"])
+    .execute();
+  return product[0].id;
+}
+
+module.exports = {
+  tearUp,
+  addUser,
+  tearDown,
+};
